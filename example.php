8c377d1b074efa52c224d8fe6b3e15eaeeb974df
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-04-24 19:30:44 +0700

    Обновлён пример, оставлены значения по-умолчанию

--- example.php
+++ example.php
@@ -9,13 +9,13 @@ use App\FishText;
 $fishtext = new FishText();
 
 // Запрашиваем данные типа Заголовок, одно предложение (по-умолчанию)
-var_dump($fishtext->title(1));
+var_dump($fishtext->title());
 
 // Устанавливаем формат возвращаемых данных: HTML
 $fishtext->setFormat('html');
 
 // Запрашиваем данные типа Предложение, по умолчанию три предложения
-var_dump($fishtext->sentence(3));
+var_dump($fishtext->sentence());
 
 // Запрашиваем данные типа Абзац, по умолчанию три абзаца
-var_dump($fishtext->paragraph(3));
+var_dump($fishtext->paragraph());
